54d16f8990c2cf69bbe0a3391cf973c668a52f36
nakedpandawashitbygit <freeekland@gmail.com>
2024-09-01 07:37:56 +0400

    Update r.php

--- r.php
+++ r.php
@@ -1,33 +1,34 @@
 <?php
 require_once 'config.php';
-
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 if ($conn->connect_error) {
     die("Ошибка подключения: " . $conn->connect_error);
 }
 
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    
-    // Получаем длинную ссылку по ID
-    $stmt = $conn->prepare("SELECT long_url FROM four WHERE id = ?");
-    $stmt->bind_param("i", $id);
+if (isset($_GET['id']) || isset($_GET['url'])) {
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $stmt = $conn->prepare("SELECT long_url FROM four WHERE id = ?");
+        $stmt->bind_param("i", $id);
+    } else if (isset($_GET['url'])) {
+        $short_url = $_GET['url'];
+        $stmt = $conn->prepare("SELECT long_url FROM four WHERE short_url = ?");
+        $stmt->bind_param("s", $short_url);
+    }
+
     $stmt->execute();
     $stmt->store_result();
     $stmt->bind_result($long_url);
     $stmt->fetch();
     
+    
+
     if ($stmt->num_rows > 0) {
         header("Location: " . $long_url);
         exit();
     } else {
-        echo "Короткая ссылка не найдена.";
+        echo "Ссылка не найдена.";
     }
-    
-    $stmt->close();
-} else {
-    echo "ID не указан.";
 }
 
 $conn->close();
