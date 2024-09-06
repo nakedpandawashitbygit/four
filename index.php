d043ad8efd980d0fd63d979e4e53a950db555f18
nakedpandawashitbygit <freeekland@gmail.com>
2024-09-06 21:28:01 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-qweqweqwe
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Вход</title>
+</head>
+<body>
+    <h1>Вход</h1>
+    <p>Вы будете перенаправлены на страницу входа...</p>
+    <script>
+        window.location.href = "login.php";
+    </script>
+</body>
+</html>
