53934ffcc0df765ce58604930ceba6740cb00076
nakedpandawashitbygit <freeekland@gmail.com>
2024-12-25 05:37:32 +0400

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Удаляет все переменные сессии
-session_destroy(); // Уничтожает сессию
-header("Location: login.php"); // Перенаправление на страницу входа
-exit();
-?>
